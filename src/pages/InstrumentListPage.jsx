15b3b373c002a380f7c5e415ce32d99e53c47c56
Maja Thunberg <majaneko@gmail.com>
2020-12-11 14:52:03 +0100

    Change path to props.match.url, return component

--- src/pages/InstrumentListPage.jsx
+++ src/pages/InstrumentListPage.jsx
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from 'react'
+import InstrumentListItem from '../components/InstrumentListItem'
 
 export default function InstrumentListPage(props) {
     const [instrumentList, setInstrumentList] = useState(null)
 
-    console.log(instrumentList)
     useEffect(()=>{
-        const path = props.match.path
+        setInstrumentList(null)
+        const path = props.match.url; // changed to convert ex "/markets/:id" to "/markets/stockholm"
+        console.log(path);
         const url = `https://market-data-collector.firebaseio.com/market-collector${path}.json`
         fetch(url)
         .then(res => res.json())
         .then(data => setInstrumentList(data))
-    },[props.match.path])
+    },[props.match.url])
 
     return (
         <div>
-            <h1>{props.match.path}</h1>
+            <h1>{props.match.url}</h1>
             {!instrumentList && <p>Loading...</p>}
 
-            {instrumentList && Object.entries(instrumentList).map((item, index)=>{            
-                return <p key={index}>{item[0]}</p>
+            {instrumentList && Object.entries(instrumentList).map((item, index)=>{     
+                return <InstrumentListItem key={index} path={props.match.url} name={item[0]}/>
             })}
 
         </div>
