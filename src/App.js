793de9bdd9070e936205e8a350eced64b893a92d
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-12-11 17:18:14 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
-import InstrumentListPage from "./pages/InstrumentListPage";
+import { Switch, Route, Link } from "react-router-dom";
 import NavLinkItem from "./components/NavLinkItem";
 import Indexes from "./pages/Indexes";
 import Markets from "./pages/Markets";
@@ -11,22 +10,38 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <ul className="nav">
-          <NavLinkItem to="/" text="Indexes" />
-          <NavLinkItem to="/markets" text="Markets" />
-          <NavLinkItem to="/currencies" text="Currencies" />
-          <NavLinkItem to="/crypto" text="Crypto" />
-        </ul>
+        <div className="col-md-12">
+          <h1>Stock Market</h1>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-12">
+          <ul className="nav">
+            <NavLinkItem to="/" text="Indexes" />
+            <NavLinkItem to="/markets" text="Markets" />
+            <NavLinkItem to="/currencies" text="Currencies" />
+            <NavLinkItem to="/crypto" text="Crypto" />
+          </ul>
+        </div>
       </div>
 
       <Switch>
-        <Route path="/"></Route>
+        <Route path="/">
+          <Indexes />
+        </Route>
 
-        <Route path="/markets"></Route>
+        <Route path="/markets">
+          <Markets />
+        </Route>
 
-        <Route path="/currencies"></Route>
+        <Route path="/currencies">
+          <Currencies />
+        </Route>
 
-        <Route path="/crypto"></Route>
+        <Route path="/crypto">
+          <Crypto />
+        </Route>
       </Switch>
     </div>
   );
