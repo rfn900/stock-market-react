eafc7d1482be7265a1e1e6e4d31fe5726a8d2e13
Maja Thunberg <majaneko@gmail.com>
2020-12-11 10:34:57 +0100

    Add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from 'react'
+import {Route, Switch, Link} from 'react-router-dom'
+import Header from './components/Header'
+import MainPage from './pages/MainPage'
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello World hello hello</h1>
+      
+      <Header />
+      
+      <Switch>
+        <Route path="/indexes">
+          <h1>Indexes page</h1>
+          
+        </Route>
+        <Route path="/markets">
+          <h1>Markets page</h1>
+          
+        </Route>
+        <Route path="/currencies">
+          <h1>Currencies page</h1>
+          
+        </Route>
+        <Route path="/crypto">
+          <h1>Crypto page</h1>
+        </Route>
+        <Route path="/"> 
+          <MainPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
