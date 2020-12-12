80b79c8c39ef138921525ea4381b5a1efb29e88d
Maja Thunberg <majaneko@gmail.com>
2020-12-12 13:39:10 +0100

    Add InstrumentDetailPage.jsx

---
+++ src/pages/InstrumentDetailPage.jsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react'
+
+export default function InstrumentDetailPage(props) {
+    const [instrument, setInstrument] = useState(null)
+
+    const hiddenInfo = ["link", "omni_ticker", "ticker"]
+
+    useEffect( () => {
+        const path = props.match.url;
+        const url = `https://market-data-collector.firebaseio.com/market-collector${path}.json`
+        // const url = "https://market-data-collector.firebaseio.com/market-collector/crypto/usd/0X-CRYPTO.json"
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setInstrument(data))
+    }, [])
+
+    return (
+        <>
+            {!instrument && <p>Loading...</p>}
+
+            {instrument && (
+                <div>
+                    {Object.entries(instrument).map((entry, index) => {
+                        if (!hiddenInfo.includes(entry[0])) {
+                            return (
+                                <p key={index}>{entry[0]}: {entry[1]}</p>
+                            )
+                        }
+                    })}
+                </div>
+            )}
+        </>
+    )
+}
