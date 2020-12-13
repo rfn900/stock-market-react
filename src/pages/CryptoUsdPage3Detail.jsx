a5456ad9a029e0aed138ee937d49b23ad3ddc974
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-12-13 20:10:13 +0100

    Delete Link

--- src/pages/CryptoUsdPage3Detail.jsx
+++ src/pages/CryptoUsdPage3Detail.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 export default function CryptoUsdPage3Detail() {
     const [cryptoObject, setCryptoObject] = useState({})
