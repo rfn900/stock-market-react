31ef6dc8bbbd29786b7a1b612d8445a462b04919
Maja Thunberg <majaneko@gmail.com>
2020-12-15 11:00:24 +0100

    Make contitional render of name

--- src/components/InstrumentListItem.jsx
+++ src/components/InstrumentListItem.jsx
@@ -7,7 +7,7 @@ export default function InstrumentListItem({path, name, text}) {
     return (
         <div>
             <Link to={`${path}/${name}`}>
-                <p>{text}</p>
+                {path === "/markets" ? <p>{name}</p> : <p>{text}</p>}
             </Link>
         </div>
     )
