a5961447251aaa6e95ed02c799d6055b027a8b51
Rodrigo Nascimento <rfn900@gmail.com>
2020-12-15 11:01:36 +0100

    fix name problem on instrumentListItem

--- src/components/InstrumentListItem.jsx
+++ src/components/InstrumentListItem.jsx
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom'
 
 
 export default function InstrumentListItem({path, name, text}) {
-
     return (
         <div>
             <Link to={`${path}/${name}`}>
-                <p>{text}</p>
+                {path === '/markets'? <p>{name}</p>:<p>{text}</p>}
             </Link>
         </div>
     )
