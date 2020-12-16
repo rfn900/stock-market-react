40afdeae60cc89d2f69f947950866626c936e543
Inna Dyachkova <inna.dyachkova@gmail.com>
2020-12-16 09:02:56 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import MainPage from './pages/MainPage'
 import InstrumentListPage from './pages/InstrumentListPage'
 import InstrumentDetailPage from './pages/InstrumentDetailPage'
 
 
 import './styles/app.scss'
+
 function App() {
   return (
     <div>
@@ -26,6 +28,9 @@ function App() {
         </Switch>
 
       </div>
+      
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    
+    return (
+        <footer className="card-footer text-muted">
+            <p>Copyright &copy;2020. All Right Reserved</p>
+        </footer>
+    )
+}
+
