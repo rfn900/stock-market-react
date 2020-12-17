566bfb4d6fce281382aeffb25de1d69c48811fff
Rodrigo Nascimento <rfn900@gmail.com>
2020-12-17 11:50:23 +0100

    Refactor Footer and FooterStyled components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
 
-const Fotnav = styled.footer`
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 4em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color:  rgba(172, 160, 187, 0.8);
-    box-shadow: -2px -6px 20px rgba(172, 160, 187, 0.2);
-    
-    p {
-        margin: 0;
-        color: rgb(247, 235, 236);
-    }
-`
+import {FooterStyled} from './FooterStyled'
 
 export default function Footer() {
     
     return (
-        <Fotnav>
+        <FooterStyled>
             <p>Copyright &copy;2020. All Right Reserved</p>
-        </Fotnav>
+        </FooterStyled>
     )
 }
 
