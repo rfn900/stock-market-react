869596261156e9129e5468e0e468ecd002a2f3e6
Maja Thunberg <majaneko@gmail.com>
2020-12-18 10:24:40 +0100

    Check if favorites in localstorage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -3,16 +3,19 @@ import Breadcrumbs from '../components/Breadcrumbs';
 import Card from '../components/Card';
 
 export default function FavoritesPage(props) {
-
-    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+    const storedFavorites = null;
+    if (localStorage.getItem("favorites")) {
+        storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+    }
     return (
         <>
             <h3>Favorites</h3>
-            <div className="row">
+            {!storedFavorites && <p>No favorites stored</p>}
+            {storedFavorites && (<div className="row">
                 {Object.entries(storedFavorites).map((item, index) => {
                     return <Card key={index} path={item[0]} ticker={item[1].ticker} name={item[1].name} isHome/>
                 })}
-            </div>
+            </div>)}
         </>
     )
 }
